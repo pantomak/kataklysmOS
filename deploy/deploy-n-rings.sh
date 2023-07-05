a17e6657d00af918d4deb50d6433aad1016ee763
fmiguel <fmiguel@outlook.com>
2023-07-05 13:32:07 +0200

    Fix scripts

--- deploy/deploy-n-rings.sh
+++ deploy/deploy-n-rings.sh
@@ -1,12 +1,18 @@
 ringcount=$1
 echo $ringcount
 
-for i in $(seq 1 $ringcount);
-do
+for i in $(seq 1 $ringcount); do
     echo "Creating ring $i..."
     kubectl create ns ring$i
+    echo "Deploying master for ring$i..."
     kubectl apply -f jmeter-master-configmap.yaml -n ring$i
     kubectl apply -f jmaster.yaml -n ring$i
+done
+
+for i in $(seq 1 $ringcount); do
+    echo "Wait for master for ring$i complete..."
+    kubectl rollout status deployment jmeter-master1 -n "ring$i" --timeout=90s
+    echo "Deploying slaves for ring$i..."
     kubectl apply -f jslaves_svc.yaml -n ring$i
     kubectl apply -f jslave.yaml -n ring$i
 done
--- deploy/reset-rings.sh
+++ deploy/reset-rings.sh
@@ -9,7 +9,6 @@ ringcount=$1
 for i in $(seq 1 $ringcount); do
     ring="ring$i"
 
-    master_pod=$(kubectl get pods -n $ring | grep jmeter-master | awk '{print $1}')
-
-    kubectl exec -n $ring -ti $master_pod bash /jmeter/apache-jmeter-5.5/bin/stoptest.sh
+    echo "Destroying ring$i..."
+    kubectl delete ns $ring --wait=false
 done
